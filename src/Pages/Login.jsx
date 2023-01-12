1c44c96acd4477b9cbd888f905f627447d2c2d69
Morning <morning@admins-iMac.local>
2023-01-12 12:47:56 +0800

    conditional added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,12 +3,17 @@ import React, { useState } from "react";
 export default function Login() {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   function loginHandler(e) {
     e.preventDefault();
     setUserName(e.target.username.value);
     setPassword(e.target.password.value);
     console.log("username is:", username);
     console.log(password);
+
+    if (username === 'Muugii' , password === 123) {
+      isLoggedIn = true;
+    }
   }
   return (
     <div className="div">
